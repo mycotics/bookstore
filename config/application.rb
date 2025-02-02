8487928aecff38c91c2ee482262166ee379a4471
Alex Cohnaiuc <acohaniuc@mixbook.com>
2025-02-03 01:24:31 +0200

    fix middle

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,66 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+class DataLayerMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, response = @app.call(env)
+    request = Rack::Request.new(env)
+
+    if Thread.current[:data_layer]
+      data_layer = Thread.current[:data_layer]
+      Thread.current[:data_layer] = nil
+      if (300..399).include?(status)
+        cookie_value = CGI.escape(data_layer.to_json)
+        add_cookie(headers, "data_layer", cookie_value)
+      else
+        headers['X-DataLayer'] = data_layer.to_json
+      end
+    else
+      if (cookie_value = request.cookies["data_layer"])
+        headers['X-DataLayer'] = cookie_value
+        if status == 200
+          delete_cookie(headers, "data_layer")
+        end
+      end
+    end
+    [status, headers, response]
+  end
+
+  private
+
+  def add_cookie(headers, name, value)
+    cookie_str = "#{name}=#{value}; path=/; HttpOnly"
+    if headers['Set-Cookie']
+      if headers['Set-Cookie'].is_a?(Array)
+        headers['Set-Cookie'] << cookie_str
+      else
+        headers['Set-Cookie'] = [headers['Set-Cookie'], cookie_str]
+      end
+    else
+      headers['Set-Cookie'] = cookie_str
+    end
+  end
+
+  def delete_cookie(headers, name)
+    cookie_str = "#{name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    if headers['Set-Cookie']
+      if headers['Set-Cookie'].is_a?(Array)
+        headers['Set-Cookie'] << cookie_str
+      else
+        headers['Set-Cookie'] = [headers['Set-Cookie'], cookie_str]
+      end
+    else
+      headers['Set-Cookie'] = cookie_str
+    end
+  end
+end
+
+
 module Bookstore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -39,9 +99,7 @@ module Bookstore
     # config.eager_load_paths << Rails.root.join('app', 'middleware')
 
     # require Rails.root.join('middleware', 'data_layer_middleware')
-
-    require Rails.root.join('app', 'middleware', 'data_layer_middleware')
-
+    
     config.middleware.use DataLayerMiddleware
 
   end
